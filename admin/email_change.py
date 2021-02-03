39f1ea9e39a7a0802e178a22987b856722f1d0e3
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-02-03 22:47:01 +0400

    replace domain

--- admin/email_change.py
+++ admin/email_change.py
@@ -7,3 +7,9 @@ def contains_domain(address,domain):
     if re.match(domain,address):
         return True
     return False    
+
+def replace_domain(address,old_domain,new_domain):
+    """replace old domain with the new one in given-address"""
+    old_domain_pattern=r'' + old_domain + '$'
+    address = re.sub(old_domain_pattern,new_domain,address)
+    return address
