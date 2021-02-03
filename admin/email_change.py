72a9559e93562b84df5c495aa751e9f987299f6d
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-02-03 22:55:04 +0400

    main

--- admin/email_change.py
+++ admin/email_change.py
@@ -13,3 +13,18 @@ def replace_domain(address,old_domain,new_domain):
     old_domain_pattern=r'' + old_domain + '$'
     address = re.sub(old_domain_pattern,new_domain,address)
     return address
+
+def main():
+    """process list of emails, replacing old domain-names"""
+    old_domain, new_domain = 'example.edu','abc.edu'
+    csv_file="/home/kali/my_file"
+    report_file='/home' + '/updated_user_emails.csv'
+    user_email_list = []
+    old_domain_email_list=[]    
+    new_domain_email_list=[]
+
+    with open(csv_file, 'r') as f:
+        user_data_list = list(csv.reader(f))
+        user_email_list = [data[1].strip() for data in user_data_list[1:]]
+
+        
