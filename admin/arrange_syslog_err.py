fafa8e1e46a6ea2959705faf3592e29557bdaf32
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-02-09 01:35:15 +0400

    sort dictionnary

--- admin/arrange_syslog_err.py
+++ admin/arrange_syslog_err.py
@@ -3,9 +3,9 @@ import sys
 import os
 import re
 import csv
-
-per_user_errors ={}
-
+import operator
+per_user_errors = {}
+per_error = {}
 with open("syslog.log") as f:
     for log in f:
       pattern=r" \((\w+)\)$"
@@ -15,9 +15,18 @@ with open("syslog.log") as f:
           if user not in per_user_errors:
               per_user_errors[user]={"ERRORS":0,"INFO":0}
           per_user_errors[user]["ERRORS"]+=1
+          error_pattern=r"[EROR]{5} .* \("
+          res=re.search(error_pattern,log)
+          error=res.group()[6:-2]
+          if error not in per_error:
+              per_error[error] = 0
+          per_error[error] += 1
       elif "INFO" in log:
           if user not in per_user_errors:
               per_user_errors[user]={"ERRORS":0,"INFO":0}
-          per_user_errors[user]["INFO"]+=1    
+          per_user_errors[user]["INFO"]+=1
           
-    print(per_user_errors)
+a=dict(sorted(per_user_errors.items(), key=lambda item:item[0]))          
+b=dict(sorted(per_error.items(), key=lambda item:item[1], reverse=True))          
+print(a)
+print(b)
