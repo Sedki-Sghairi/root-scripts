882445f62ea9fc072613a6c59f39a9c335efadfe
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-02-11 18:20:58 +0400

    Return True if the root partition is full, False otherwise.

--- admin/checks.py
+++ admin/checks.py
@@ -17,3 +17,6 @@ def check_disk_full(disk, min_gb, min_percent):
         return True
     return False    
 
+def check_root_full():
+    return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
