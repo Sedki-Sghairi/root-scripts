03d156fbc8fbb1c3b78737a6b7e114c07514a565
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-02-13 16:29:46 +0400

    reboot server

--- admin/monitor_website.py
+++ admin/monitor_website.py
@@ -1,10 +1,16 @@
 import requests
 import smtplib
 import os
+from linode_api4 import linodeClient, Instance
+
+ACESS_TOKEN=os.environ.get('LINODE_TOKEN')
+ID=os.environ.get('SERVER_INSTANCE_ID')
 EMAIL=os.environ.get('MY_EMAIL')
 EMAIL_RECIEVE=os.environ.get('MY_OTHER_EMAIL')
 PASS=os.environ.get('MY_PASS')
+
 r = requests.get('https://www.sedkisghairi.com', timeout=5)
+
 if r.status_code != 200:
     with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
         smtp.ehlo()
@@ -15,4 +21,8 @@ if r.status_code != 200:
         body='Site is Down, restart the server.'
         msg=f'Subject: {subject}\n\n{body}'
         smtp.sendmail(EMAIL,EMAIL_RECIEVE,msg)
+        
+    client=linodeClient(ACESS_TOKEN)
+    my_server = client.load(Instance, ID)
+    my_server.reboot()
 
